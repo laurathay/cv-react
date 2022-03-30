f4ce078ec09ff1d9184eadb73a4060c3aa73d42e
Laura THAY <laura.thay@ipag.fr>
2022-03-30 22:47:43 +0200

    les routes des pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import  { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Knowledges from './pages/Knowledges';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <div>
-      
+        <BrowserRouter>
+          <Route path="/" component={Home} />
+          <Route path="/competences" component={Knowledges} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </BrowserRouter>
     </div>
   );
 };
