7ad5ef25f5740efc01106ef4d9f5f21cbce44276
Laura THAY <laura.thay@ipag.fr>
2022-04-01 19:28:13 +0200

    Mayday Switch obsolete
    same pour React Dom Rout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import  { BrowserRouter, Route, Switch } from 'react-router-dom';
-// loool 
+import  { BrowserRouter, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Knowledges from './pages/Knowledges';
 import Portfolio from './pages/Portfolio';
@@ -11,13 +11,11 @@ const App = () => {
   return (
     <div>
         <BrowserRouter>
-          <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/competences" component={Knowledges} />
             <Route path="/portfolio" component={Portfolio} />
             <Route path="/contact" component={Contact} />
             <Route component={NotFound} />
-          </Switch>
         </BrowserRouter>
     </div>
   );
