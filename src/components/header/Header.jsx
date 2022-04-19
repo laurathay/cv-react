a29ac8cac45ba7e2585251f4b279a299e5fa6bc0
Laura THAY <laura.thay@ipag.fr>
2022-04-19 20:43:20 +0200

    header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.jpg'
+import ME from '../../assets/me.jpeg'
 import HeaderSocial from './HeaderSocial'
 
 const Header = () => {
@@ -13,7 +13,7 @@ const Header = () => {
                 <h5 className="text-light"> Web design integrator/ developer </h5>
                 <CTA />
                 <HeaderSocial />
-                
+
                 <div className="me">
                     <img src={ME} alt=""/>
                 </div>
