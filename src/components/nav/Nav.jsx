98168373bc3937cbf75756dd3066a130f3fefa82
Laura THAY <laura.thay@ipag.fr>
2022-04-21 18:48:11 +0200

    nav baar icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {FaHome} from 'react-icons/fa'
+import {FiUser} from 'react-icons/fi'
+import {BsBook} from 'react-icons/bs'
+import {BiMessageRoundedDots} from 'react-icons/bi'
+import {MdComputer} from 'react-icons/md'
+
 
 const Nav = () => {
     return (
-        <div>
-            <p> Navigation </p>
-        </div>
+        <nav>
+            <a href="#"><FaHome /></a>
+            <a href="#"><FiUser /></a>
+            <a href="#"><BsBook /></a>
+            <a href="#"><MdComputer /></a>
+            <a href="#"><BiMessageRoundedDots /></a>
+        </nav>
     )
 }
 
