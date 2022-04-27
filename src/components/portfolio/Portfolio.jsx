80562a0fbabd9a03f0d85b481c239428c6cee432
Laura THAY <laura.thay@ipag.fr>
2022-04-27 18:20:23 +0200

    portfolio html done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,68 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio.png'
 
 const Portfolio = () => {
     return (
         <section id='portfolio'>
-            <p> Portfolio</p>
+            <h5>My recent work</h5>
+            <h3> Portfolio</h3>
+
+            <div className="container portfolio__container">
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={IMG1} alt=""/>
+                    </div>
+                    <h3> We are all cucumber with anxiety</h3>
+                    <a href="http://github.com" className="btn"> Github </a>
+                    <a href="http://github.com" className="btn btn-primary" target='_blank'> LiveDemo </a>
+                </article>
+
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                    <img src={IMG1} alt=""/>
+                    </div>
+                    <h3> We are all cucumber with anxiety</h3>
+                    <a href="http://github.com" className="btn"> Github </a>
+                    <a href="http://github.com" className="btn btn-primary" target='_blank'> LiveDemo </a>
+                </article>
+
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                    <img src={IMG1} alt=""/>
+                    </div>
+                    <h3> We are all cucumber with anxiety</h3>
+                    <a href="http://github.com" className="btn"> Github </a>
+                    <a href="http://github.com" className="btn btn-primary" target='_blank'> LiveDemo </a>
+                </article>
+
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                    <img src={IMG1} alt=""/>
+                    </div>
+                    <h3> We are all cucumber with anxiety</h3>
+                    <a href="http://github.com" className="btn"> Github </a>
+                    <a href="http://github.com" className="btn btn-primary" target='_blank'> LiveDemo </a>
+                </article>
+
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                    <img src={IMG1} alt=""/>
+                    </div>
+                    <h3> We are all cucumber with anxiety</h3>
+                    <a href="http://github.com" className="btn"> Github </a>
+                    <a href="http://github.com" className="btn btn-primary" target='_blank'> LiveDemo </a>
+                </article>
+
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                    <img src={IMG1} alt=""/>
+                    </div>
+                    <h3> We are all cucumber with anxiety</h3>
+                    <a href="http://github.com" className="btn"> Github </a>
+                    <a href="http://github.com" className="btn btn-primary" target='_blank'> LiveDemo </a>
+                </article>
+            </div>
         </section>
     )
 }
